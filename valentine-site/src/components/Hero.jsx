30196746e3efe234c980d060a20f6129f7465be5
Yatharth Patel <pyatharth41@gmail.com>
2026-02-14 15:04:47 +0530

    Fixed

--- valentine-site/src/components/Hero.jsx
+++ valentine-site/src/components/Hero.jsx
@@ -85,6 +85,8 @@ export default function Hero() {
           variants={fadeUp}
           className="mt-6 text-sm text-gray-600"
         >
+          My little <span className="text-blue-500">Penguin 🐧</span>,  
+          my soft <span className="text-pink-500">Panda 🐼</span>
         </motion.p>
 
         <motion.p
